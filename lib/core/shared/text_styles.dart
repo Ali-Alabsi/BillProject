42aa928b202e09d47ff9f9c231b143ce8d321f59
DESKTOP-DQFVKJD\alabsi <AliAlabsi>
2025-01-06 22:55:51 +0300

    Desgin Page Login And Signup

---
+++ lib/core/shared/text_styles.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+
+class TextStyles{
+  static TextStyle font22mainColorW600 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+    color: ProjectColors.mainColor
+  );
+  static TextStyle font24mainColorW700 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    color: ProjectColors.mainColor
+  );
+  static TextStyle font16BlackBold = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: ProjectColors.blackColor
+  );
+  static TextStyle font18BlackBold = TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      color: ProjectColors.blackColor
+  );
+  static TextStyle font16BlackW500 = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      color: ProjectColors.blackColor
+  );
+  static TextStyle font16mainColorBold = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: ProjectColors.mainColor
+  );
+
+  static TextStyle font16grayColorW300 = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w300,
+      color: ProjectColors.greyColor
+  );
+  static TextStyle font16mainColorW300 = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w300,
+      color: ProjectColors.mainColor
+  );
+  static TextStyle font16whiteColorW300 = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w300,
+      color: ProjectColors.whiteColor
+  );
+  static TextStyle font14grayColorW400 = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w300,
+      color: ProjectColors.greyColor
+  );
+  static TextStyle font14mainColorW400 = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w300,
+      color: ProjectColors.mainColor
+  );
+  static TextStyle font18whiteW600 = TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: ProjectColors.whiteColor
+  );
+}
